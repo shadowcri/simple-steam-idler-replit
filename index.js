1a8f1e0832c018c8b25687bd433a1bb3d46b0e5e
shadowcri <156939400+shadowcri@users.noreply.github.com>
2024-04-06 02:41:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [2357570, 730, 1056640, 1966720, 1237950, 1449850, 714010, 1238840];  // Enter here AppIDs of the needed games
+var games = [203770, 42960, 310950, 431960, 1237950, 730, 438100, 2881650];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 
 
